45e7bdbb55d8640abb89b4b1669d452a61987a0f
Ashenmist712 <jaelleyte12.03@gmail.com>
2026-03-15 16:32:00 -0600

    preparacion de la maqueta de la tabla de reportesCliente

---
+++ public/js/reportesCliente/reportesCliente.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+$('#tablaReporteClienteLoad').load('reportesCliente/tablaReportesCliente.php');
+});
--- vistas/misReportes.php
+++ vistas/misReportes.php
@@ -5,12 +5,16 @@ if (isset($_SESSION['usuario']) && $_SESSION['usuario']['rol'] == 1) {
 
 ?>
 
-    <!-- Page Content -->
     <div class="container">
         <div class="card border-0 shadow my-5">
             <div class="card-body p-5">
-                <h1 class="fw-light">Reportes</h1>
-                <p class="lead">Content on the page will</p>
+                <h1 class="fw-light">Reportes de Cliente</h1>
+                <p class="lead">
+                    <button class="btn btn-primary" data-toggle="modal" data-target="#modalCrearReporte">Crear Reporte</button>
+                    <hr>
+                <div id="#tablaReporteClienteLoad"></div>
+
+                </p>
             </div>
         </div>
     </div>
@@ -18,7 +22,12 @@ if (isset($_SESSION['usuario']) && $_SESSION['usuario']['rol'] == 1) {
 
     </html>
 
-<?php include_once 'footer.php';
+    <?php
+    include 'reportesCliente/modalCrearReporte.php';
+    include_once 'footer.php';
+    ?>
+    <script src="../public/js/reportesCliente/reportesCliente.js"></script>
+<?php
 } else {
     header('Location: ../index.html');
 }
